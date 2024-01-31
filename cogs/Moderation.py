5ec6810a9218a193a107ba7109cef4e676800914
Aman <amanclementaranha@gmail.com>
2024-01-31 23:07:59 +0530

    managing roles ?

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,10 +1,40 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
+from discord.utils import get
 
 class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
+    
+    @commands.command(pass_Context = True)
+    @has_permissions(manage_roles = True)
+    async def addroles(self, ctx, user : discord.Member , *, role : discord.Role):
+        if role in user.roles:
+            await ctx.send(f'{user} already hsa the role ,{role}')
+        else :
+            await user.add_roles(role)
+            await ctx.send(f'Added {role} to {user}')
+    
+    @addroles.error
+    async def addrole_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Sit down you do not have permissions')
+    
+    @commands.command(pass_Context = True)
+    @has_permissions(manage_roles = True)
+    async def removeroles(self, ctx, user : discord.Member , *, role : discord.Role):
+        if role in user.roles:
+            await user.remove_roles(role)
+            await ctx.send(f'{role} was removed from {user}')
+        else :
+            await ctx.send(f'{user} does not have the role {role}')
+    
+    @removeroles.error
+    async def removerole_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Sit down you do not have permissions')
+    
     
     @commands.Cog.listener()
     async def on_message(self, message):
